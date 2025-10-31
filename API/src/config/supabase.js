04e98e0eb67b5524b77dfd1dca82a23264a1f6ca
Valeria Gomez <vagomez@estudiantec.cr>
2025-10-30 19:14:16 -0600

    Database update

--- API/src/config/supabase.js
+++ API/src/config/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://aiyuoqjwzevphzjpymgs.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFpeXVvcWp3emV2cGh6anB5bWdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ0ODE0MjgsImV4cCI6MjA2MDA1NzQyOH0.Uu79K31NK3RCK40XRBQ_X1vbaYxx3yene0Tiwf-cXno';
+const supabaseUrl = 'https://opptodvvohelldblkuws.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9wcHRvZHZ2b2hlbGxkYmxrdXdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE4NTcwMjQsImV4cCI6MjA3NzQzMzAyNH0.TuBzGTx4uULRqyONn7FjU-sYw9SiHexfivkDMayRsAc';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
