9ddd4702d8eb6700383d73c90cfbfb68d45492ca
Valeria Gómez <100319615+gomezv24@users.noreply.github.com>
2025-11-26 21:53:50 -0600

    Update autenticacion.js

--- frontend/src/functions/autenticacion.js
+++ frontend/src/functions/autenticacion.js
@@ -1,6 +1,6 @@
 export async function Autenticar(valores) {
   try {
-    const temp = await fetch("http://localhost:5000/usuarios/login", {
+    const temp = await fetch("https://disenno-inw6.vercel.app/usuarios/login", {
       method: 'POST',
       body: JSON.stringify(valores),
       headers: {
